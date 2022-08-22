c5a7bf4eae3b2fda60f8f08fe0ebba847d5bab00
Vy Kim Nguyen <z5254704@unsw.edu.au>
2022-08-22 20:39:40 +1000

    Added social media icons and links, made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./../styles/Navbar.css"
+import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
 
 function Navbar() {
   return (
@@ -19,6 +20,15 @@ function Navbar() {
         </li>
         <li className="item">
           <a href="/contact">Contact</a>
+        </li>
+        <li className="item icon">
+          <a href="https://www.instagram.com/kimvy3201/" target="_blank"><BsInstagram /></a>
+        </li>
+        <li className="item icon">
+          <a href="https://www.linkedin.com/in/vykim-nguyen/" target="_blank"><BsLinkedin /></a>
+        </li>
+        <li className="item icon">
+          <a href="https://github.com/kiminim" target="_blank"><BsGithub /></a>
         </li>
       </div>
     </nav>
