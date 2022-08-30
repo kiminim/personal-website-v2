02894124e2e90be183ad6f24ee363c484e1a934a
Vy Kim Nguyen <z5254704@ad.unsw.edu.au>
2022-08-30 11:37:26 +0700

    Added toggle functionalty to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import React from "react"
+import { useState } from "react"
 import "./../styles/Navbar.css"
 import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
     <nav className="navigation">
       <a href="/" className="logo">
         Vy Kim Nguyen
       </a>
-      <div className="nav-menu" id="my-nav">
+      <a className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+          <GiHamburgerMenu />
+      </a>
+      <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
         <a href="/about">About</a>
         <a href="/projects">Projects</a>
         <a href="/media">Media</a>
@@ -17,21 +22,9 @@ function Navbar() {
         <a className="icon" href="https://www.instagram.com/kimvy3201/" target="_blank"><BsInstagram /></a>
         <a className="icon" href="https://www.linkedin.com/in/vykim-nguyen/" target="_blank"><BsLinkedin /></a>
         <a className="icon" href="https://github.com/kiminim" target="_blank"><BsGithub /></a>
-        <a href="javascript:void(0);" class="hamburger" onclick="myFunction()">
-          <GiHamburgerMenu />
-        </a>
       </div>
     </nav>
   )
-}
-
-export function myFunction() {
-  var x = document.getElementById("my-nav");
-  if (x.className === "nav-menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-menu";
-  }
 }
 
 export default Navbar;
