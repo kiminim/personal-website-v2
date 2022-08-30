9f44d53949287765cf6701a9bf9cc8fece67ea2b
Vy Kim Nguyen <z5254704@ad.unsw.edu.au>
2022-08-30 21:25:06 +0700

    added greeting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,6 +6,11 @@ function Home() {
   return (
     <div className="intro">
       <img className="portrait" src={profile}/>
+      <p className="greeting">
+        Hi there! I'm Kim, a 4th year bioinformatics and biomedical engineering student @ UNSW.
+        <br/>
+        This is my website to showcase my projects, photography, nail art and more!
+      </p>
     </div>
   )
 }
