d554cc202c54919fa8e6eb7855b40fb568ef3ddb
Vy Kim Nguyen <z5254704@ad.unsw.edu.au>
2022-08-30 20:32:05 +0700

    Added expanded nav for smaller screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,17 +11,28 @@ function Navbar() {
       <a href="/" className="logo">
         Vy Kim Nguyen
       </a>
-      <a className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+      <a 
+        className="hamburger" 
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
           <GiHamburgerMenu />
       </a>
-      <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
-        <a href="/about">About</a>
-        <a href="/projects">Projects</a>
-        <a href="/media">Media</a>
-        <a href="/contact">Contact</a>
-        <a className="icon" href="https://www.instagram.com/kimvy3201/" target="_blank"><BsInstagram /></a>
-        <a className="icon" href="https://www.linkedin.com/in/vykim-nguyen/" target="_blank"><BsLinkedin /></a>
-        <a className="icon" href="https://github.com/kiminim" target="_blank"><BsGithub /></a>
+      <div
+        className={
+          isNavExpanded ? "nav-menu expanded" : "nav-menu"
+        }
+      >
+        <ul>
+          <li><a href="/about">About</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/media">Media</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a className="icon" href="https://www.instagram.com/kimvy3201/" target="_blank" rel="noopener noreferrer"><BsInstagram /></a></li>
+          <li><a className="icon" href="https://www.linkedin.com/in/vykim-nguyen/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a></li>
+          <li><a className="icon" href="https://github.com/kiminim" target="_blank" rel="noopener noreferrer"><BsGithub /></a></li>
+        </ul>
       </div>
     </nav>
   )
