aaaebbba104065e950807a4f0edad6fd76d94527
Vy Kim Nguyen <z5254704@ad.unsw.edu.au>
2022-08-31 11:28:00 +0700

    Added button

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,25 @@
 import React from "react"
 import "./../../styles/Home.css"
 import profile from "./../images/DSC_0944.jpg"
+import { AiOutlineArrowRight } from "react-icons/ai"
 
 function Home() {
   return (
     <div className="intro">
       <img className="portrait" src={profile}/>
-      <p className="greeting">
-        Hi there! I'm Kim, a 4th year bioinformatics and biomedical engineering student @ UNSW.
-        <br/>
-        This is my website to showcase my projects, photography, nail art and more!
-      </p>
+      <div className="text">
+        <p className="greeting">
+          Hi there! I'm Kim, a 4th year bioinformatics and biomedical engineering student @ UNSW.
+          <br/><br/>
+          This is my website to showcase my programming projects, photography, nail art and more!
+        </p>
+        <div className="button">
+          <a href="/about">
+            More about me
+            <AiOutlineArrowRight className="arrow"/>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
